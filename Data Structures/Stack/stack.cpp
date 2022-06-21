2b197d2311f59a754cc8c6111eac81ad54dacc73
White Ant <trafi227@gmail.com>
2022-06-21 12:17:43 +0600

    .

--- Data Structures/Stack/stack.cpp
+++ Data Structures/Stack/stack.cpp
@@ -1,15 +1,42 @@
 #include<iostream>
 using namespace std;
 
-
+#define max 100
 class stack{
 
+    int top;
 
+    public:
+        int stk[max];
+        stack(){ top = - 1; } //constructor ja top define kore dibo
 
+        bool push(int item);    //jodi stack max na hoy taile push korbo naile false return korbo
+
+        int pop();              //jodi khali na thake taile pop korbo uprer item naile 
+        bool empty();
 
 };
 
+bool stack::push(int item){
 
+    if(top >= (max-1)){
+        return false;
+    }else{
+        
+        stk[++top] = item;
+        return true;
+    }
+
+}
+
+
+int stack::pop(){
+    
+
+
+
+
+}
 
 
 
