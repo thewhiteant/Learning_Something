f341a4e9003391d172aa1f5a5646b3f03e74bf22
White Ant <trafi227@gmail.com>
2022-07-07 15:28:29 +0600

    .

---
+++ Algorithm/Extra/Max/MAXN/maxn.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+
+using namespace std;
+   
+int maxo(int ar[],int n){
+
+      int temp = ar[0];
+      for (int i = 0; i < n; i++)
+      {
+              if(temp < ar[i] ){
+        temp = ar[i];
+      }}     
+return temp; 
+
+}
+
+int mino(int ar[],int n){
+
+      int temp = ar[0];
+      for (int i = 0; i < n; i++)
+      {
+              if(temp > ar[i] ){
+        temp = ar[i];
+      }}     
+return temp; 
+
+}
+
+char maxoc(int ar[],int n){
+
+      int temp = ar[0];
+      for (int i = 0; i < n; i++)
+      {
+              if(temp < ar[i] ){
+        temp = ar[i];
+      }}     
+return temp; 
+
+}
+
+char minoc(int ar[],int n){
+
+      int temp = ar[0];
+      for (int i = 0; i < n; i++)
+      {
+              if(temp > ar[i] ){
+        temp = ar[i];
+      }}     
+return temp; 
+
+}
+
+
+
+int main(){
+
+int k[5] = {100,12,45,450,52};
+char ck[5] = {'g','d','s','a','l'};
+
+cout<<maxo(k,5)<<endl;
+cout<<mino(k,5)<<endl;
+
+
+cout<<maxoc(ck,5)<<endl;
+cout<<minoc(ck,5)<<endl;
+
+
+
+    return 0;
+}
